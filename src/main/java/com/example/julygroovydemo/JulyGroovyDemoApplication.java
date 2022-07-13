b55d995601a7dc67d989f675fa8f10e1c97a3b14
kennethhowell <howell.kenneth.a@gmail.com>
2022-07-13 09:29:39 -0500

    pushing initial project structure

---
+++ src/main/java/com/example/julygroovydemo/JulyGroovyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.julygroovydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyGroovyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulyGroovyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/julygroovydemo/JulyGroovyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.julygroovydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyGroovyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
